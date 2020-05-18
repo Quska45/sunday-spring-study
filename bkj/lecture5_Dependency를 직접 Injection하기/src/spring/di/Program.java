c323b0c1741ed6c6a25d5c3e3afc0e146769eed9
GwangJin Byeon <qusrhkdwls45@gmail.com>
2020-05-18 23:12:02 +0900

    fix : 8강 스프링으로 DI 구현

--- bkj/lecture5_Dependency를 직접 Injection하기/src/spring/di/Program.java
+++ bkj/lecture5_Dependency를 직접 Injection하기/src/spring/di/Program.java
@@ -1,18 +1,31 @@
 package spring.di;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import spring.di.entity.Exam;
 import spring.di.entity.NewLecExam;
 import spring.di.ui.ExamConsole;
 import spring.di.ui.GridExamConsole;
 import spring.di.ui.InlineExamConsole;
+import sun.awt.windows.awtLocalization_ko;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Exam exam = new NewLecExam();
-		ExamConsole console = new InlineExamConsole(exam);
-//		ExamConsole console = new GridExamConsole(exam);
+//		스프링에 지시하는 방법으로 변경
+//		Exam exam = new NewLecExam();
+//		ExamConsole console = new GridExamConsole();
+//		
+//		console.setExam(exam);
+//		
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring/di/setting.xml");
+		
+		//이름으로 꺼내오면 형식을 Object로 꺼내오기 때문에 형변환을 해줘야 한다.
+		ExamConsole console  = (ExamConsole) context.getBean("console");
+//		ExamConsole console  = context.getBean(ExamConsole.class, "console");
 		console.print();
 	}
 
--- bkj/lecture5_Dependency를 직접 Injection하기/src/spring/di/ui/GridExamConsole.java
+++ bkj/lecture5_Dependency를 직접 Injection하기/src/spring/di/ui/GridExamConsole.java
@@ -6,14 +6,20 @@ public class GridExamConsole implements ExamConsole {
 
 	private Exam exam;
 	
-	public GridExamConsole(Exam exam) {
-		this.exam = exam;
+	public GridExamConsole() {
+		// TODO Auto-generated constructor stub
 	}
 	
 	@Override
 	public void print() {
 		// TODO Auto-generated method stub
-		
+		System.out.printf("grid total is %d, avg is %f\n", exam.total(), exam.avg());
+	}
+
+	@Override
+	public void setExam(Exam exam) {
+		// TODO Auto-generated method stub
+		this.exam = exam;
 	}
 
 }
