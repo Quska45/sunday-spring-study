62f867bcc1d77a9f2d2f656c6a151eeb799e756e
GwangJin Byeon <qusrhkdwls45@gmail.com>
2020-06-10 23:36:46 +0900

    feat : react 간단하게 테스트하고 주석도 달아놓음

--- bkj/react-and-spring/frontend/src/App.js
+++ bkj/react-and-spring/frontend/src/App.js
@@ -2,6 +2,9 @@ import React, {Component, useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//헤더
+import mainHeader from './component/mainHeader.js';
+
 function App () {
     const [message, setMessage] = useState("");
     const [chageMessage, setChangeMessage] = useState(0);
@@ -10,6 +13,8 @@ function App () {
     //여기서는 message라는 상태가 변경될 때 스프링으로 실행된 서버에서 /api/hello경로로 이동해 데이터를 받아서 message에서 값을 받아온다.
     //배열인자에 값을 넣어주면  useEffect를 message가 변경될 때 마다 실행 시킬 수 있다.
     useEffect(() => {
+        //proxy 설정이 되어 있기 때문에 전체 경로를 쓰지 않아도 된다.
+        //cors관련 오류를 간단하게 방지 할 수 있기 때문에 설정을 해놓고 쓰는 것이 좋겠다. 여기서 전체 경로를 쓰면 데이터를 제대로 받아오지 못한다.
         fetch('/api/hello')
             .then(response => response.text())
             .then(message => {
@@ -19,6 +24,7 @@ function App () {
     
     return (
         <div className="App">
+        <mainHeader>123</mainHeader>
         <header className="App-header">
         <img src={logo} className="App-logo" alt="logo"/>
         <h1 className="App-title">{message}</h1>
---
+++ bkj/react-and-spring/frontend/src/component/mainHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const mainHeader = props => {
+  return (
+    <div>
+      헤더 입니다.
+    </div>
+  );
+};
+
+export default mainHeader;
