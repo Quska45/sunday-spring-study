fcfe8c3207dbdc4c4ffabc60da692531ed550ae1
GwangJin Byeon <qusrhkdwls45@gmail.com>
2020-07-02 00:12:43 +0900

    feat : react-and-spring프로젝트에 몽고디비 연동

--- bkj/react-and-spring/src/main/java/no/kantega/springandreact/HelloController.java
+++ bkj/react-and-spring/src/main/java/no/kantega/springandreact/HelloController.java
@@ -1,7 +1,13 @@
 package no.kantega.springandreact;
 
+import java.io.IOException;
 import java.util.Date;
+import java.util.Iterator;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +16,19 @@ public class HelloController {
     @RequestMapping("/api/hello")
     public String hello() {
         return "Hello,12321 the time at the server is now " + new Date() + "\n";
+    }
+    
+    @RequestMapping("/api/crawling")
+    public void crawling() throws IOException {
+    	System.out.println(123);
+    	String URL = "https://www.zerocho.com/";
+    	Document doc = Jsoup.connect(URL).get();
+    	Elements elem = doc.select("li");
+    	
+    	Iterator<Element> ie1 = elem.select("b").iterator();
+		
+		while(ie1.hasNext()) {
+			System.out.println(ie1.next().text());
+		}
     }
 }
